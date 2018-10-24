3af63f37de929b543cc6e69e741943440e6687ba
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-10-25 09:26:53 +1100

    no message

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        [Key]
         public int ID { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
--- RazorPagesMovie/Pages/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Index.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        MovieContext _movieContext;
+
+        public IndexModel(MovieContext movieContext)
         {
+            _movieContext = movieContext;
+
+        }
+        public async void OnGet()
+        {
+            var x = _movieContext.Movie.ToList();
+
+            for (int i = 0; i < 500; i++)
+            {
+                _movieContext.Movie.Add(new Movie
+                {
+                    Genre = $"test {i}",
+                    Price = 10m,
+                    ReleaseDate = DateTime.Now,
+                    Title = "test"
+                });
+
+                await _movieContext.SaveChangesAsync();
+
+            }
 
         }
     }
